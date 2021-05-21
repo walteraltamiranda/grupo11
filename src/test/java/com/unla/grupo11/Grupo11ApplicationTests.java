970d41e46074e55d37c886a9781fff16b7845e5b
walteraltamiranda <walter.altamiranda@gmail.com>
2021-05-20 22:13:37 -0300

    Adhiriendo spring files

---
+++ src/test/java/com/unla/grupo11/Grupo11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unla.grupo11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
